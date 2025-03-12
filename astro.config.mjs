e7e4a5f2bca8cc87aeca7b41bcea9b323acffdf4
Alex Aspinall <alex.aspinall@blueyonder.co.uk>
2025-03-12 09:57:37 +0000

    Deployment settings for Github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alexaspinalldev.github.io/alexaspinall',
+  // base: '/alexaspinall',
   vite: {
     plugins: [tailwindcss()]
   }
