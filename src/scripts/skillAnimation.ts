8d1766296bd100ef95f3bc9bf01d577974e5564d
Alex Aspinall <alex.aspinall@blueyonder.co.uk>
2025-03-13 19:33:00 +0000

    Disable animation on visibility change

--- src/scripts/skillAnimation.ts
+++ src/scripts/skillAnimation.ts
@@ -1,15 +1,13 @@
-document.addEventListener('visibilitychange', () => {
-  !document.hidden ? skillAnimation() : null;
-});
+// document.addEventListener('visibilitychange', () => {!document.hidden ? skillAnimation() : null; });
 
 async function skillAnimation() {
   const skills = document.querySelectorAll('.skillCont svg');
 
   for (const skill of skills) {
-      await new Promise(resolve => setTimeout(resolve, 100));
-      skill.classList.toggle('skillHover');
-      await new Promise(resolve => setTimeout(resolve, 300));
-      skill.classList.toggle('skillHover');
+    await new Promise(resolve => setTimeout(resolve, 100));
+    skill.classList.toggle('skillHover');
+    await new Promise(resolve => setTimeout(resolve, 300));
+    skill.classList.toggle('skillHover');
   }
   await new Promise(resolve => setTimeout(resolve, 15000));
   skillAnimation(); // Loops
