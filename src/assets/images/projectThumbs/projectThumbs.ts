22ed18edbe6e83dac9d1975946cf44b51432fb11
Alex Aspinall <alex.aspinall@blueyonder.co.uk>
2025-03-15 23:02:55 +0000

    Add Messageboard project card

--- src/assets/images/projectThumbs/projectThumbs.ts
+++ src/assets/images/projectThumbs/projectThumbs.ts
@@ -1,5 +1,6 @@
 export const projectThumbs = {
     "dana-lbg": import("./dana-lbg.webp"),
     "terra-java": import("./terra-java.png"),
+    "message-board": import("./message-board-header.png"),
     // Other images
 }
