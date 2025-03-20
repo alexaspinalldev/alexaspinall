4f20950446718a9db480fe216f46a0ca3ece0f19
Alex Aspinall <alex.aspinall@blueyonder.co.uk>
2025-03-20 15:29:00 +0000

    Fix issue with skill animation

--- src/scripts/skillAnimation.ts
+++ src/scripts/skillAnimation.ts
@@ -1,14 +1,28 @@
-// document.addEventListener('visibilitychange', () => {!document.hidden ? skillAnimation() : null; });
+// document.addEventListener('visibilitychange', () => { !document.hidden ? skillAnimation() : null; });
+document.addEventListener('DOMContentLoaded', () => skillAnimation());
 
 async function skillAnimation() {
-  const skills = document.querySelectorAll('.skillCont svg');
+  const skills = document.querySelectorAll('.skillCont svg'); // Select all SVG elements
+  console.log("Animating skills");
 
   for (const skill of skills) {
+    const svgSkill = skill as SVGSVGElement; // Cast to SVGSVGElement
+
     await new Promise(resolve => setTimeout(resolve, 100));
-    skill.classList.toggle('skillHover');
-    await new Promise(resolve => setTimeout(resolve, 300));
-    skill.classList.toggle('skillHover');
+
+    // Apply effect
+    svgSkill.style.filter = 'none';
+    svgSkill.classList.toggle('scale-110');
+    svgSkill.classList.toggle('drop-shadow-xl');
+
+    await new Promise(resolve => setTimeout(resolve, 400));
+
+    // Remove effect
+    svgSkill.style.filter = 'grayscale(100%)';
+    svgSkill.classList.toggle('scale-110');
+    svgSkill.classList.toggle('drop-shadow-xl');
   }
+
   await new Promise(resolve => setTimeout(resolve, 15000));
   skillAnimation(); // Loops
 }
